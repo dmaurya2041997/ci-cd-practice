d44619648ecc4c88c7e5a20bf11b8659e517a58d
dmaurya2041997 <dharmendra.maurya@machinelearningtech.ai>
2026-05-02 10:07:25 +0530

    component added

---
+++ src/components/UnusedComponent.tsx
@@ -0,0 +1,12 @@
+
+
+const UnusedComponent: React.FC = () => {
+  return (
+    <div>
+      <h1>Unused Component</h1>
+      <p>This component is not currently being used.</p>
+    </div>
+  );
+};
+
+export default UnusedComponent;
